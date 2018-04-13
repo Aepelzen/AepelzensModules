71d955fc7c941d26167669db8611373fa76da52b
Aepelzen <aepelzen@localhost.localdomain>
2018-04-13 22:07:35 +0200

    Rework Filter

--- src/AeFilter.hpp
+++ src/AeFilter.hpp
@@ -13,7 +13,7 @@ struct AeFilter {
     float a0, a1, a2, b0, b1, b2;
 
     float process(float in) {
-	float out = b0/a0 * in + b1/a0 * x[0] + b2/a0 * x[1] - a1/a0 * y[0] - a2/a0 * y[1];
+	float out = b0 * in + b1 * x[0] + b2 * x[1] - a1 * y[0] - a2 * y[1];
 
 	//shift buffers
 	x[1] = x[0];
@@ -32,20 +32,21 @@ struct AeFilter {
 
 	switch(type) {
 	case AeLOWPASS:
-	    b0 =  (1 - cs0)/2;
-	    b1 =   1 - cs0;
-	    b2 =  (1 - cs0)/2;
 	    a0 =   1 + alpha;
-	    a1 =  -2*cs0;
-	    a2 =   1 - alpha;
+
+	    b0 = (1 - cs0) /2 /a0;
+	    b1 = (1 - cs0) /a0;
+	    b2 = (1 - cs0) /2 /a0;
+	    a1 = (-2 * cs0) /a0;
+	    a2 = (1 - alpha)/a0;
 	    break;
 	case AeHIGHPASS:
-	    b0 =  (1 + cs0)/2;
-	    b1 = -(1 + cs0);
-	    b2 =  (1 + cs0)/2;
-	    a0 =   1 + alpha;
-	    a1 =  -2*cs0;
-	    a2 =   1 - alpha;
+	    a0 = 1 + alpha;
+	    b0 = (1 + cs0)/2 /a0;
+	    b1 = -(1 + cs0) /a0;
+	    b2 = (1 + cs0) /2 /a0;
+	    a1 = -2 * cs0 /a0;
+	    a2 = (1 - alpha) /a0;
 	}
     }
 };
@@ -60,7 +61,7 @@ struct AeFilterFrame : AeFilter {
     Frame<CHANNELS> process(Frame<CHANNELS> in) {
 	Frame<CHANNELS> out;
 	for(int i=0;i<channels;i++) {
-	    out.samples[i] = b0/a0 * in.samples[i] + b1/a0 * x[0].samples[i] + b2/a0 * x[1].samples[i] - a1/a0 * y[0].samples[i] - a2/a0 * y[1].samples[i];
+	    out.samples[i] = b0 * in.samples[i] + b1 * x[0].samples[i] + b2 * x[1].samples[i] - a1 * y[0].samples[i] - a2 * y[1].samples[i];
 
 	    //shift buffers
 	    x[1].samples[i] = x[0].samples[i];
@@ -73,24 +74,25 @@ struct AeFilterFrame : AeFilter {
 };
 
 struct AeFilterStereo : AeFilter {
-    //first index for channel, second for shift
-    float x[2][2];
-    float y[2][2];
+    float xl[2];
+    float xr[2];
+    float yl[2];
+    float yr[2];
 
     void process(float* inL, float* inR) {
-	float l = b0/a0 * *inL + b1/a0 * x[0][0] + b2/a0 * x[0][1] - a1/a0 * y[0][0] - a2/a0 * y[0][1];
-	float r = b0/a0 * *inR + b1/a0 * x[1][0] + b2/a0 * x[1][1] - a1/a0 * y[1][0] - a2/a0 * y[1][1];
+	float l = b0 * *inL + b1 * xl[0] + b2 * xl[1] - a1 * yl[0] - a2 * yl[1];
+	float r = b0 * *inR + b1 * xr[0] + b2 * xr[1] - a1 * yr[0] - a2 * yr[1];
 
 	//shift buffers
-	x[0][1] = x[0][0];
-	x[0][0] = *inL;
-	x[1][1] = x[1][0];
-	x[1][0] = *inR;
+	xl[1] = xl[0];
+	xl[0] = *inL;
+	xr[1] = xr[0];
+	xr[0] = *inR;
 
-	y[0][1] = y[0][0];
-	y[0][0] = l;
-	y[1][1] = y[1][0];
-	y[1][0] = r;
+	yl[1] = yl[0];
+	yl[0] = l;
+	yr[1] = yr[0];
+	yr[0] = r;
 
 	*inL = l;
 	*inR = r;
